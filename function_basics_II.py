17869b61d28e74fe424f13135adfd760a7a53afe
shawnljohnson1980 <78113657+shawnljohnson1980@users.noreply.github.com>
2021-04-05 21:57:55 -0400

    completed assignment

---
+++ function_basics_II.py
@@ -0,0 +1,40 @@
+def count_down():
+    #     i = []
+    #     for i in range(1):
+    #         i = [*range(5, -1, -1)]
+    #         print(i)
+    #     return i
+
+    # count_down()
+    # def myList(a, b):
+    #     print(a)
+    #     return(b)
+
+    # def firstPlusLenght():
+    #     list1 = [2, 3, 4, 5, 6]
+    #     print(list1[0] + len(list1))
+
+    # firstPlusLenght()
+
+    # myList(4, 8)
+    # def valuesGreatThanSecond():
+    #     list = [5, 2, 3, 2, 1, 4]
+    #     print([i for i in list if i > 2])
+
+    # valuesGreatThanSecond()
+
+    # if I want the index of 0 to be the new legth and the last value to be the value of all indexes
+    # first I state a function the the list variable.
+    #  I need index of 0 to equal the new length
+
+    # def this_length_that_value(list2):
+    #     list1 = []
+    #     list2 = []
+    #     for i in list1(0, 1, 1):
+    #         while i < len(list1):
+    #             list1.append(list1[1])
+    #             list1[1] == list2
+    #         return list2
+
+    # print(this_length_that_value([4, 7]))
+    #
